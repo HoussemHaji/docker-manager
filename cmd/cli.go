7e7e186db6d0b109c21047fdfce8a42d4130a366
Houssem Haji <houssem.haji@insat.ucar.tn>
2024-09-01 17:46:49 +0100

    implement entry point

---
+++ cmd/cli.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"docker-cli-tool/internal/ui"
+
+	"github.com/rivo/tview"
+)
+
+// initialize cli and start app
+func StartCli() {
+	app := tview.NewApplication()
+	table := ui.CreateTable(app)
+
+	flex := tview.NewFlex().AddItem(table, 0, 1, true)
+
+	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
+		panic(err)
+	}
+}
